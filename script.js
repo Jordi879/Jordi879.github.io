2f808695200fd4ec55153bba4556a907fc8dc34d
Jordi MARTINEZ <jordi.martinez@bticino.com>
2024-12-07 17:47:48 -0600

    Agregar -landing page- V3 Funcional

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const GOOGLE_SHEETS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxblh_zYowMTpvZEBB6mOuk2BthipthxiQNkQCfq7CH8YAASs6GhJ-_aMj1QwEAy5jq/exec';
 
 // Elementos del DOM
+const welcomeModal = document.getElementById('welcome-modal');
+const continueModalBtn = document.getElementById('continue-modal-btn');
 const landingPage = document.getElementById('landing-page');
 const formPage = document.getElementById('form-page');
 const continueBtn = document.getElementById('continue-btn');
@@ -11,6 +13,22 @@ const clickCountElement = document.getElementById('click-count');
 
 // Variables para contar
 let clickCount = 0;
+window.onload = function() {
+    welcomeModal.style.display = 'flex'; // Mostrar el modal
+};
+continueModalBtn.addEventListener('click', () => {
+    welcomeModal.style.display = 'none'; // Ocultar el modal
+    landingPage.style.display = 'block'; // Mostrar la página de contenido
+});
+continueBtn.addEventListener('click', () => {
+    landingPage.style.display = 'none';
+    formPage.style.display = 'block';
+    
+    // Incrementar y mostrar contador de clicks
+    clickCount++;
+    clickCountElement.textContent = `Número de clicks: ${clickCount}`;
+});
+
 
 // Registrar visita al cargar la página
 fetch(GOOGLE_SHEETS_SCRIPT_URL, {
@@ -22,7 +40,17 @@ fetch(GOOGLE_SHEETS_SCRIPT_URL, {
     body: JSON.stringify({
         action: 'registerVisit'
     })
+})
+.then(response => {
+    if (response.ok) {
+        console.log("Visita registrada correctamente.");
+    } 
+})
+.catch(error => {
+    console.log("Error en la solicitud:", error);
 });
+
+
 
 // Evento para cambiar a la página de formulario
 continueBtn.addEventListener('click', () => {
@@ -37,13 +65,16 @@ continueBtn.addEventListener('click', () => {
     fetch(GOOGLE_SHEETS_SCRIPT_URL, {
         method: 'POST',
         mode: 'no-cors',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            action: 'registerClick'
-        })
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ action: 'registerClick' })
+    }).then(response => {
+        if (response.ok) {
+            // Mensaje de éxito
+        } else {
+            // Mensaje de error
+        }
     });
+    
 });
 
 // Evento para manejar el envío del formulario
